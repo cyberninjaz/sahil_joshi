7b1ea93a97fef9d87c7b6e412bc6220f715a99ae
sahiljoshi1 <52418056+sahiljoshi1@users.noreply.github.com>
2019-07-01 11:53:35 -0400

    Add files via upload

--- SahilGame.py
+++ SahilGame.py
@@ -1,3 +1,4 @@
+print("Welcome to the game. Whenever you are choosing an option, please type in the exact choice")
 class Choice:
     description = ""
     destination = ""
@@ -15,11 +16,11 @@ class Decisionpoint:
         self.choices=c
 
 dictionary = {
-    "start": Decisionpoint( "WHERE WILL YOU GO?",  [
-        Choice("Go to Houston", "Houston"),
-        Choice("Or Go to El Paso", "El Paso")  
+    "start": Decisionpoint( "Where will you go?",  [
+        Choice("Go to houston", "houston"),
+        Choice("Or go to el paso", "el paso")  
     ] ),
-    "Houston": Decisionpoint("What Will You do?", [
+    "houston": Decisionpoint("What Will You do?", [
         Choice("talk to stranger","talk to stranger"),
         Choice("Or talk to police?","talk to police")
     ] ),
@@ -41,16 +42,51 @@ dictionary = {
     ]),
     "west": Decisionpoint("After searching for 5 days, you finally found the treasure!", [
     ]),   
-    "El Paso": Decisionpoint("What Will You do?", [
+    "el paso": Decisionpoint("What Will You do?", [
         Choice("talk to person","talk to person"),
-        Choice("Or Go shopping?","go shopping")  
+        Choice("Or go shopping?","go shopping")  
     ]),   
     "talk to person": Decisionpoint("person robs you and kills you", [
        
     ]),    
     "go shopping": Decisionpoint("What Will You buy?", [
-        Choice("Buy Clothes","buy clothes"),
-        Choice("or Buy Food?","buy food")
+        Choice("buy clothes","buy clothes"),
+        Choice("or buy food?","buy food")
+    ]),    
+    "buy clothes": Decisionpoint("Stranger aproaches you and kills you for your clothes", [
+
+    ]),
+     "buy food": Decisionpoint("You find a stranger, what do you do?", [
+        Choice("give him Food", "give him food"),
+        Choice("or keep your food","keep your food")
+    ]),
+    "keep your food": Decisionpoint("Stranger gets mad and kills you", [
+    ]),
+    "give him food": Decisionpoint("Stranger likes you and asks you to come home with him, what do you do?", [
+        Choice("go home","go home"),
+        Choice("or don't go home", "don't go home")
+    ]),
+    "don't go home": Decisionpoint("Stranger gets mad and kills you", [
+    ]),
+    "go home": Decisionpoint("At the strangers house, you find a mysterious item. what do you do?", [
+        Choice("leave", "leave"),
+        Choice("Or steal the item", "steal the item")
+    ]),  
+     "leave": Decisionpoint("Stranger gets mad and kills you", [
+    ]),
+    "steal the item": Decisionpoint("The stranger doesn't notice you stealing his item, what do you do?", [
+        Choice("leave", "leave"),
+        Choice("or stay","stay")
+    ]),    
+    "leave": Decisionpoint("Stranger gets mad and kills you", [
+    ]),
+    "stay": Decisionpoint("The stranger goes to the bathroom, what do you do?", [
+        Choice("leave", "leave"),
+        Choice("or use the mysterious item","use the mysterious item")
+    ]),   
+    "leave": Decisionpoint("Stranger gets mad and kills you", [
+    ]),
+     "use the mysterious item": Decisionpoint("The mysterious item kills you", [
     ])
     }     
 
